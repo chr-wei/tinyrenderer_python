68a776bbfc44572e0e55b75d9dcbf19d1da38bae
Christian Weihsbach <rrap1ide@gmail.com>
2020-09-04 23:03:02 +0200

    Add texture draw methods skeletons

--- our_gl.py
+++ our_gl.py
@@ -1,17 +1,15 @@
 from collections import namedtuple
 from operator import attrgetter
 from tiny_image import TinyImage
+from model import get_texture_color
+
 import random
 import numpy
 import PIL
-# Tuple definitions
 
+# Tuple definitions
 Point = namedtuple ("Point", "x y")
 Vertex = namedtuple("Vertex", "x y z")
-VertexIds = namedtuple("VertexIds", "id_one id_two id_three")
-TangentIds = namedtuple("TangentIds", "id_one id_two id_three")
-NormalIds = namedtuple("NormalIds", "id_one id_two id_three")
-FacedataIds = namedtuple("FacedataIds", "VertexIds TangentIds NormalIds")
 
 BoundingBox = namedtuple("BoundingBox", "x_min y_min z_min x_max y_max z_max")
 
@@ -196,10 +194,7 @@ def draw_rasterized_triangle(p0: Point, p1: Point, p2: Point, image: TinyImage, 
             (one_uv, u, v) = barycentric(p0, p1, p2, Point(x,y))
             if one_uv >= 0 and u >= 0 and v >= 0:
                 image.set(x, y, color)
-                # Error: Problem with pixels lying on the edge of the triangle - too less triangles drawn
     return image
-
-
 
 def draw_zbuffered_triangle(v0: Vertex, v1: Vertex, v2: Vertex, zbuffer: list, image: TinyImage, color):
     points = [v0, v1, v2]
@@ -220,7 +215,35 @@ def draw_zbuffered_triangle(v0: Vertex, v1: Vertex, v2: Vertex, zbuffer: list, i
                 if z > zbuffer[x][y]:
                     zbuffer[x][y] = z
                     image.set(x, y, color)
-                    # Error: Problem with pixels lying on the edge of the triangle - too less triangles drawn
+    return image
+
+def draw_textured_triangle(v0: Vertex, v1: Vertex, v2: Vertex, zbuffer: list, 
+                           p0: Point, p1: Point, p2: Point, texture_image : TinyImage, shading_factor: float, 
+                           image: TinyImage):
+    points = [v0, v1, v2]
+
+    points.sort(key=attrgetter('x'))
+    x_min = points[0].x
+    x_max = points[2].x
+
+    points.sort(key=attrgetter('y'))
+    y_min = points[0].y
+    y_max = points[2].y
+
+    for x in range(x_min, x_max+1):
+        for y in range(y_min, y_max+1):
+            (one_uv, u, v) = barycentric(Point(v0.x, v0.y), Point(v1.x, v1.y), Point(v2.x, v2.y), Point(x,y))
+            if one_uv >= 0 and u >= 0 and v >= 0:
+                z = one_uv*v0.z + u * v1.z + v * v2.z
+                p_texture = one_uv*p0 + u * p1 + v *p2
+
+                if z > zbuffer[x][y]:
+                    zbuffer[x][y] = z
+
+                    # Get texture color
+                    color = get_texture_color(texture_image, p_texture.x, p_texture.y)
+
+                    image.set(x, y, color)
     return image
 
 
@@ -289,4 +312,55 @@ def draw_flat_shaded_meshtriangles(face_id_data, vertices, bounding_box, image):
 
         # image = draw_rasterized_triangle(Point(x0, y0), Point(x1, y1), Point(x2, y2), image, color)##8.1
         image = draw_zbuffered_triangle(Vertex(x0, y0, v0.z),  Vertex(x1, y1, v1.z), Vertex(x2, y2, v2.z), zbuffer, image, color)##8.2
+    return image
+
+
+def draw_textured_mesh(face_id_data, vertices, bounding_box, texture_points, texture_image, image):
+
+    x_shift = (bounding_box.x_max + bounding_box.x_min) / 2
+    y_shift = (bounding_box.y_max + bounding_box.y_min) / 2
+
+    x_scale = image.width / (bounding_box.x_max - bounding_box.x_min)
+    y_scale = image.height / (bounding_box.y_max - bounding_box.y_min)
+
+    scale = min(x_scale, y_scale) * .8
+
+    print("Drawing " + str(len(face_id_data)) + " triangles ...")
+    
+    w, h = image.get_width(), image.get_height()
+    zbuffer = [[-float('Inf') for bx in range(w)] for y in range(h)] #8.2 
+
+    for face in face_id_data.values():
+        vert_ids = face.VertexIds
+        v0 = vertices[vert_ids.id_one]
+        v1 = vertices[vert_ids.id_two]
+        v2 = vertices[vert_ids.id_three]
+
+        texture_pt_ids = face.TexturePointIds
+        p0 = texture_points[texture_pt_ids.id_one]
+        p1 = texture_points[texture_pt_ids.id_two]
+        p2 = texture_points[texture_pt_ids.id_three]
+        
+        # Calculating color shading
+        n = cross_product(Vertex(*numpy.subtract(v0, v1)) , Vertex(*numpy.subtract(v2, v0)))
+        cos_phi = scalar(normalize(n), normalize(light_dir))
+
+        if cos_phi < 0:
+            continue
+        
+        color = int(255 * cos_phi)
+        color = (color, color, color)
+
+        x0 = int((v0.x-x_shift)*scale + w /2)
+        y0 = int((v0.y-y_shift)*scale + h / 2)
+
+        x1 = int((v1.x-x_shift)*scale + w /2)
+        y1 = int((v1.y-y_shift)*scale + h / 2)
+
+        x2 = int((v2.x-x_shift)*scale + w / 2)
+        y2 = int((v2.y-y_shift)*scale + h / 2)
+
+        image = draw_textured_triangle(Vertex(x0, y0, v0.z),  Vertex(x1, y1, v1.z), Vertex(x2, y2, v2.z), zbuffer,
+                                       Point(p0.x, p0.y), Point(p1.x, p1.y), Point(p2.x, p2.y), texture_image, cos_phi, 
+                                       image)
     return image
