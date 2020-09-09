acc89f13a571a821b98b7fef310c3ac479064a5e
Christian Weihsbach <weihsbach.c@gmail.com>
2020-09-09 22:06:58 +0200

    Remove duplicate and unnecessary functions

--- main.py
+++ main.py
@@ -1,76 +1,7 @@
-import sys
-
 from tiny_image import TinyImage
 import our_gl as gl
 
 from model import get_model_face_ids, get_vertices, get_model_texture_points
-
-#Excercises
-#
-#
-def excercise_point():
-    """Draw a simple point"""
-    image = TinyImage(100, 100)
-    image.set(50,30, "red") ##1.1
-    image.save_to_disk("out.png")
-
-def excercise_lines():
-    """Draw a simple line"""
-    image = TinyImage(200, 200)
-
-    image = gl.draw_line((0,0), (100,20), image, "white")##2.1
-    image = gl.draw_line((40,100), (100, 29), image, "white")##2.2
-    # image = draw_line((0,0), (image.width-1,image.height-1), image, "white")##2.3
-    image.save_to_disk("out.png")
-
-def excercise_triangles():
-    """Draw triangle edges"""
-    image = TinyImage(200, 200)
-    image = gl.draw_triangle((3,5), (20,100), (110,50), image, "white")##3.1
-    image.save_to_disk("out.png")
-
-def excercise_mesh(obj_filename, output_filename):
-    """Draw mesh unfilled mesh triangles"""
-    image = TinyImage(2000, 2000)
-        
-    print("Reading facedata ...")
-    face_id_data = get_model_face_ids(obj_filename)
-
-    print("Reading vertices ...")
-    vertices, bounding_box = get_vertices(obj_filename)
-
-    gl.draw_meshtriangles(face_id_data, vertices, bounding_box, image, "white")##6
-
-    image.save_to_disk(output_filename)
-        
-def excercise_filled_triangles():
-    """Draw some simple filled triangles"""
-    image = TinyImage(300, 300)
-    p1 = gl.Point(100, 5)
-    p2 = gl.Point(100, 150)
-    p3 = gl.Point(200, 50)
-
-    image = gl.draw_filled_triangle(p1, p2, p3, image, "white")##5.1
-    image = gl.draw_triangle(p1, p2, p3, image, "red")##5.1
-
-    # p4 = Point(20, 240)##5.2
-    # p5 = Point(180, 199)##5.2
-    # image = draw_filled_triangle(p2, p4, p5, image, "white")##5.2
-    image.save_to_disk("out.png")
-
-def excercise_filled_mesh(obj_filename, output_filename):
-    """Draw a filled mesh with random facet colors"""
-    image = TinyImage(2000, 2000)
-        
-    print("Reading facedata ...")
-    face_id_data = get_model_face_ids(obj_filename)
-
-    print("Reading vertices ...")
-    vertices, bounding_box = get_vertices(obj_filename)
-
-    gl.draw_filled_meshtriangles(face_id_data, vertices, bounding_box, image)
-
-    image.save_to_disk(output_filename)
 
 def excercise_textured_mesh(obj_filename, texture_filename, output_filename):
     """Draw a filled mesh with random facet colors"""
@@ -94,49 +25,5 @@ def excercise_textured_mesh(obj_filename, texture_filename, output_filename):
 
     image.save_to_disk(output_filename)
 
-def excercise_rasterized_triangle():
-    """Draw a simple filled triangle using rasterization"""
-    # import numpy##7.1
-    # Test cross product
-    # v1 = Vertex(1,2,3)##7.1
-    # v2 = Vertex(4,5,6)##7.1
-    # print(cross_product(v1,v2))##7.1
-    # print(numpy.cross(v1,v2))##7.1
-
-    image = TinyImage(300, 300)
-    p0 = gl.Point(50, 5)
-    p1 = gl.Point(200, 10)
-    p2 = gl.Point(100,  250)
-
-
-    image = gl.draw_ref_triangle(p0, p1, p2, image, "red")#7.2
-    image = gl.draw_rasterized_triangle(p0, p1, p2, image, "white")#7.2
-    image.save_to_disk("out.png")
-
-def excercise_flat_shaded_mesh(obj_filename, output_filename):
-    """Draw a mesh with flat shading: Grey shaded triangles depending on angle between surface normal and light direction"""
-    image = TinyImage(2000, 2000)
-        
-    print("Reading facedata ...")
-    face_id_data = get_model_face_ids(obj_filename)
-
-    print("Reading vertices ...")
-    vertices, bounding_box = get_vertices(obj_filename)
-
-    gl.draw_flat_shaded_meshtriangles(face_id_data, vertices, bounding_box, image)
-
-    image.save_to_disk(output_filename)
-
 if __name__ == "__main__":
-
-    # excercise_point()##1
-    # excercise_lines()##2
-    # excercise_triangles()##3
-    # excercise_mesh("obj/autumn.obj", "autumn.png")##4
-    # excercise_filled_triangles()##5
-    # excercise_filled_mesh("obj/spring_autumn.obj", "spring_autumn.png")##6.1
-    # excercise_filled_mesh("obj/autumn.obj", "autumn.png")##6.2
-    # excercise_rasterized_triangle()##7
-    # excercise_flat_shaded_mesh("obj/autumn.obj", "autumn.png")##8.1 and 8.2
-    # excercise_textured_mesh("obj/head.obj", "obj/african_head_diffuse.tga", "out.png")##9.1 and 9.2
     excercise_textured_mesh("obj/autumn.obj", None, "autumn.png")##9.1 and 9.2
--- our_gl.py
+++ our_gl.py
@@ -37,7 +37,6 @@ def draw_line(p0, p1, image, color):
     else:
         steep_line = False
 
-
     if x0 == x1:
         # Due to switching steep lines this only occurs if y0 == y1 and x0 == x1
         # Only draw p0 dot in this case
@@ -58,169 +57,13 @@ def draw_line(p0, p1, image, color):
 
     return image
 
-def draw_triangle(p0, p1, p2, image, color):
+def draw_triangle_lines(p0, p1, p2, image, color):
     image = draw_line(p0, p1, image, color)
     image = draw_line(p1, p2, image, color)
     image = draw_line(p2, p0, image, color)
     return image
 
-    
-def draw_ref_triangle(p0, p1, p2, image, color):
-    image._draw.line((p0.x, p0.y, p1.x, p1.y), fill = color)
-    image._draw.line((p0.x, p0.y, p2.x, p2.y), fill = color)
-    image._draw.line((p1.x, p1.y, p2.x, p2.y), fill = color)
-    return image
-
-def draw_meshtriangles(face_id_data, vertices, bounding_box, image, color):
-
-    x_shift = (bounding_box.x_max + bounding_box.x_min) / 2
-    y_shift = (bounding_box.y_max + bounding_box.y_min) / 2
-
-    x_scale = image.width / (bounding_box.x_max - bounding_box.x_min)
-    y_scale = image.height / (bounding_box.y_max - bounding_box.y_min)
-
-    scale = min(x_scale, y_scale) * .8
-
-    print("Drawing " + str(len(face_id_data)) + " triangles ...")
-    
-    for face in face_id_data.values():
-        vert_ids = face.VertexIds
-        v0 = vertices[vert_ids.id_one]
-        v1 = vertices[vert_ids.id_two]
-        v2 = vertices[vert_ids.id_three]
-
-        x0 = int((v0.x-x_shift)*scale + image.get_width() /2)
-        y0 = int((v0.y-y_shift)*scale + image.get_height() / 2)
-
-        x1 = int((v1.x-x_shift)*scale + image.get_width() /2)
-        y1 = int((v1.y-y_shift)*scale + image.height / 2)
-
-        x2 = int((v2.x-x_shift)*scale + image.get_width() /2)
-        y2 = int((v2.y-y_shift)*scale + image.get_height() / 2)
-
-        image = draw_triangle(Point(x0, y0), Point(x1, y1), Point(x2, y2), image, color)
-    return image
-
-def draw_filled_meshtriangles(face_id_data, vertices, bounding_box, image):
-
-    x_shift = (bounding_box.x_max + bounding_box.x_min) / 2
-    y_shift = (bounding_box.y_max + bounding_box.y_min) / 2
-
-    x_scale = image.width / (bounding_box.x_max - bounding_box.x_min)
-    y_scale = image.height / (bounding_box.y_max - bounding_box.y_min)
-
-    scale = min(x_scale, y_scale) * .8
-
-    print("Drawing " + str(len(face_id_data)) + " triangles ...")
-    
-    for face in face_id_data.values():
-        vert_ids = face.VertexIds
-        v0 = vertices[vert_ids.id_one]
-        v1 = vertices[vert_ids.id_two]
-        v2 = vertices[vert_ids.id_three]
-
-        x0 = int((v0.x-x_shift)*scale + image.get_width() /2)
-        y0 = int((v0.y-y_shift)*scale + image.get_height() / 2)
-
-        x1 = int((v1.x-x_shift)*scale + image.get_width() /2)
-        y1 = int((v1.y-y_shift)*scale + image.height / 2)
-
-        x2 = int((v2.x-x_shift)*scale + image.get_width() /2)
-        y2 = int((v2.y-y_shift)*scale + image.get_height() / 2)
-
-        colors = ["cyan", "gray", "lightblue", "orange", "purple"]
-        # image = draw_filled_triangle(Point(x0, y0), Point(x1, y1), Point(x2, y2), image, 
-        #    random.choice(colors))##5
-        image = draw_rasterized_triangle(Point(x0, y0), Point(x1, y1), Point(x2, y2), image, 
-            random.choice(colors))##7
-    return image
-
-def draw_filled_triangle(p0: Point, p1: Point, p2: Point, image: TinyImage, color):
-    image = draw_triangle(p0,p1,p2, image, color)
-    
-    sorted_points = [p0, p1, p2]
-    sorted_points.sort(key=attrgetter('x'))
-
-    edge_main = (sorted_points[0], sorted_points[2])
-    edge_left = (sorted_points[0], sorted_points[1])
-    edge_right = (sorted_points[1], sorted_points[2])
-
-    x0_main = edge_main[0].x
-    y0_main = edge_main[0].y
-    x1_main = edge_main[1].x
-    y1_main = edge_main[1].y
-
-    for x_sweep in range(x0_main + 1, x1_main):
-        if x_sweep < edge_left[1].x:
-            x0_minor = edge_left[0].x
-            y0_minor = edge_left[0].y
-            x1_minor = edge_left[1].x
-            y1_minor = edge_left[1].y
-        else:
-            x0_minor = edge_right[0].x
-            y0_minor = edge_right[0].y 
-            x1_minor = edge_right[1].x
-            y1_minor = edge_right[1].y
-        
-        if x1_main == x0_main:
-            y_main = y1_main
-        else:
-            y_main = int(y0_main + (y1_main-y0_main) / (x1_main-x0_main) * (x_sweep-x0_main) + .5)
-
-        if x1_minor == x0_minor:
-            y_minor = y0_minor
-        else:
-            y_minor = int(y0_minor + (y1_minor-y0_minor) / (x1_minor-x0_minor) * (x_sweep-x0_minor) + .5)
-
-        if (y_minor > y_main):
-            # Make y_main greater than y_minor
-            (y_main, y_minor) = (y_minor, y_main)
-        
-        for y in range(y_minor+1, y_main):
-            image.set(x_sweep, y, color)
-
-    return image
-
-def draw_rasterized_triangle(p0: Point, p1: Point, p2: Point, image: TinyImage, color):
-    points = [p0, p1, p2]
-
-    points.sort(key=attrgetter('x'))
-    x_min = points[0].x
-    x_max = points[2].x
-
-    points.sort(key=attrgetter('y'))
-    y_min = points[0].y
-    y_max = points[2].y
-
-    for x in range(x_min, x_max+1):
-        for y in range(y_min, y_max+1):
-            (one_uv, u, v) = barycentric(p0, p1, p2, Point(x,y))
-            if one_uv >= 0 and u >= 0 and v >= 0:
-                image.set(x, y, color)
-    return image
-
-def draw_zbuffered_triangle(v0: Vertex, v1: Vertex, v2: Vertex, zbuffer: list, image: TinyImage, color):
-    points = [v0, v1, v2]
-
-    points.sort(key=attrgetter('x'))
-    x_min = points[0].x
-    x_max = points[2].x
-
-    points.sort(key=attrgetter('y'))
-    y_min = points[0].y
-    y_max = points[2].y
-
-    for x in range(x_min, x_max+1):
-        for y in range(y_min, y_max+1):
-            (one_uv, u, v) = barycentric(Point(v0.x, v0.y), Point(v1.x, v1.y), Point(v2.x, v2.y), Point(x,y))
-            if one_uv >= 0 and u >= 0 and v >= 0:
-                z = one_uv*v0.z + u * v1.z + v * v2.z
-                if z > zbuffer[x][y]:
-                    zbuffer[x][y] = z
-                    image.set(x, y, color)
-    return image
-
-def draw_textured_triangle(v0: Vertex, v1: Vertex, v2: Vertex, zbuffer: list, 
+def draw_triangle(v0: Vertex, v1: Vertex, v2: Vertex, zbuffer: list, 
                            p0: Point, p1: Point, p2: Point, texture_image : TinyImage, shading_factor: float, 
                            image: TinyImage):
     points = [v0, v1, v2]
@@ -276,50 +119,6 @@ def normalize(v0:Vertex):
 def scalar(v0:Vertex, v1:Vertex):
     return v0.x*v1.x + v0.y*v1.y + v0.z*v1.z
 
-def draw_flat_shaded_meshtriangles(face_id_data, vertices, bounding_box, image):
-
-    x_shift = (bounding_box.x_max + bounding_box.x_min) / 2
-    y_shift = (bounding_box.y_max + bounding_box.y_min) / 2
-
-    x_scale = image.width / (bounding_box.x_max - bounding_box.x_min)
-    y_scale = image.height / (bounding_box.y_max - bounding_box.y_min)
-
-    scale = min(x_scale, y_scale) * .8
-
-    print("Drawing " + str(len(face_id_data)) + " triangles ...")
-    
-    w, h = image.get_width(), image.get_height()
-    zbuffer = [[-float('Inf') for bx in range(w)] for y in range(h)] #8.2 
-
-    for face in face_id_data.values():
-        vert_ids = face.VertexIds
-        v0 = vertices[vert_ids.id_one]
-        v1 = vertices[vert_ids.id_two]
-        v2 = vertices[vert_ids.id_three]
-        
-        # Calculating color shading
-        n = cross_product(Vertex(*np.subtract(v0, v1)) , Vertex(*np.subtract(v2, v0)))
-        cos_phi = scalar(normalize(n), normalize(light_dir))
-
-        if cos_phi < 0:
-            continue
-        
-        color = int(255 * cos_phi)
-        color = (color, color, color)
-
-        x0 = int((v0.x-x_shift)*scale + w /2)
-        y0 = int((v0.y-y_shift)*scale + h / 2)
-
-        x1 = int((v1.x-x_shift)*scale + w /2)
-        y1 = int((v1.y-y_shift)*scale + h / 2)
-
-        x2 = int((v2.x-x_shift)*scale + w / 2)
-        y2 = int((v2.y-y_shift)*scale + h / 2)
-
-        # image = draw_rasterized_triangle(Point(x0, y0), Point(x1, y1), Point(x2, y2), image, color)##8.1
-        image = draw_zbuffered_triangle(Vertex(x0, y0, v0.z),  Vertex(x1, y1, v1.z), Vertex(x2, y2, v2.z), zbuffer, image, color)##8.2
-    return image
-
 def draw_textured_mesh(face_id_data : dict, vertices : dict, bounding_box : BoundingBox, 
                        texture_points : dict, texture_image : TinyImage, 
                        image : TinyImage):
@@ -374,7 +173,7 @@ def draw_textured_mesh(face_id_data : dict, vertices : dict, bounding_box : Boun
         x2 = int((v2.x-x_shift)*scale + w / 2)
         y2 = int((v2.y-y_shift)*scale + h / 2)
 
-        image = draw_textured_triangle(Vertex(x0, y0, v0.z),  Vertex(x1, y1, v1.z), Vertex(x2, y2, v2.z), zbuffer,
+        image = draw_triangle(Vertex(x0, y0, v0.z),  Vertex(x1, y1, v1.z), Vertex(x2, y2, v2.z), zbuffer,
                                        p0, p1, p2, texture_image, cos_phi, 
                                        image)
     return image
