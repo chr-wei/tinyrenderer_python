61adbcfa9e6634dc235e4e89c27ed6a4aacc2afb
Christian Weihsbach <rrap1ide@gmail.com>
2020-09-27 17:43:52 +0200

    Refactor output property config

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ from model import get_model_face_ids, get_vertices, get_model_texture_points
 
 if __name__ == "__main__":
     
-    draw = 0
-    if draw == 0:
+    # Model property selection
+    model_prop_set = 0
+    if model_prop_set == 0:
         obj_filename = "obj/autumn.obj"
         texture_filename = "obj/TEX_autumn_body_color.png"
         output_filename = "out.png"
 
-    elif draw == 1:
+    elif model_prop_set == 1:
         obj_filename = "obj/autumn.obj"
         texture_filename = None
         output_filename = "out.png"
@@ -24,8 +25,31 @@ if __name__ == "__main__":
         obj_filename = "obj/head.obj"
         texture_filename = "obj/african_head_diffuse.tga"
         output_filename = "out.png"
+    
+    # Image property selection
+    img_prop_set = 0
+    if img_prop_set == 0:
+        (w, h) = (2000, 2000)
+    else:
+        (w, h) = (800, 800)
 
-    image = TinyImage(2000, 2000)
+    image = TinyImage(w, h)
+
+    # View property selection
+    view_prop_set = 0
+    if view_prop_set == 0:
+        eye = Vector_3D(0, 0, 1) # Lookat camera 'eye' position
+        center = Vector_3D(0, 0, 0) # Lookat 'center'. 'eye' looks at center
+        up = Vector_3D(0, 1, 0) # Camera 'up' direction
+        scale = .8 # Viewport scaling
+    else:
+        eye = Vector_3D(0, 0, 1) # Lookat camera 'eye' position
+        center = Vector_3D(0, 0, 0) # Lookat 'center'. 'eye' looks at center
+        up = Vector_3D(0, 1, 0) # Camera 'up' direction
+        scale = .8 # Viewport scaling
+
+    # Light property
+        light_dir = Vector_3D(0, -1, -1)
 
     if texture_filename is None:
         texture_image = None
@@ -44,30 +68,30 @@ if __name__ == "__main__":
 
     print("Drawing triangles ...")
     
-    w, h = image.get_width(), image.get_height()
     zbuffer = [[-float('Inf') for bx in range(w)] for y in range(h)]
+
+    # Define tranformation matrices
 
     # Generate model transformation matrix which transforms vertices according to the model bounding box
     # min[-1, -1, -1] to max[1, 1, 1] object space
-    M_model = gl.model(bbox[0], bbox[1])
+    M_model = gl.model_transform(bbox[0], bbox[1])
 
+    # Generate cam transformation
+    M_lookat = gl.lookat(eye, center, up)
 
-    M_lookat = gl.lookat(Vector_3D(0, 0, 1), 
-                      Vector_3D(0, 0, 0), 
-                      Vector_3D(0, 1, 0))
-
-    M_modelview = M_lookat * M_model
-
+    # Generate perspective transformation
     M_perspective = gl.perspective(4.0)
-    
-    scale = .8
+
+    # Generate transformation to final viewport
     M_viewport = gl.viewport(+scale*w/8, +scale*h/8, scale*w, scale*h, 255)
 
+    # Combine matrices
+    M_modelview = M_lookat * M_model
     M = M_viewport * M_perspective * M_modelview
     
-    light_dir = Vector_3D(0, -1, -1)
     light_dir = (M_modelview * light_dir.expand_4D_vect()).project_3D()
 
+    # Iterate model faces
     for face in progressbar(face_id_data, ):
         vert_ids = face.VertexIds
 
