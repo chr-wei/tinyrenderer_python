5806e2119c240e7dc7975377b98aa244e2151ecb
Christian Weihsbach <rrap1ide@gmail.com>
2020-09-27 19:44:24 +0200

    Implement Model_Storage class and add Gouraud shader

--- main.py
+++ main.py
@@ -4,8 +4,38 @@ from progressbar import progressbar
 from tiny_image import TinyImage
 
 import our_gl as gl
-from geom import Vector_3D, Matrix_3D, matmul, cross_product, transform_vertex
-from model import get_model_face_ids, get_vertices, get_model_texture_points
+from geom import Matrix_4D, Matrix_3D, Vector_3D, matmul, cross_product, transform_vertex
+from model import Model_Storage, get_model_face_ids, get_vertices
+
+class Gouraud_Shader(gl.Shader):
+    mdl: Model_Storage
+    varying_intensity = [None] * 3 # Written by vertex shader, read by fragment shader
+    light_dir: Vector_3D
+    M_viewport: Matrix_4D
+    M_projection: Matrix_4D
+    M_view: Matrix_4D
+    M_model: Matrix_4D
+
+    def Gouraud_Shader(self, mdl, light_dir, M_viewport, M_projection, M_view, M_model):
+        self.mdl = mdl
+        self.light_dir = light_dir
+        self.M_viewport = M_viewport
+        self.M_projection = M_projection
+        self.M_view = M_view
+        self.M_model = M_model
+
+    def vertex(self, face_idx: int, vert_idx: int):
+        self.varying_intensity[vert_idx] = max(0, self.mdl.get_normal(face_idx, vert_idx)*light_dir) # Get diffuse lighting intensity
+        vertex = self.mdl.get_vertex(face_idx, vert_idx) # Read the vertex
+        
+        return self.M_viewport * self.M_projection * self.M_view * self.M_model * vertex.expand_4D_point() # Transform it to screen coordinates
+
+    def fragment(self, barycentric: tuple, color: Vector_3D):
+        intensity = self.varying_intensity[0]*barycentric[0] \
+                  + self.varying_intensity[1]*barycentric[1] \
+                  + self.varying_intensity[2]*barycentric[2] # Interpolate intensity for the current pixel
+        color = Vector_3D(255, 255, 255)*intensity // 1
+        return (False, color) # Do not discard pixel and return color
 
 if __name__ == "__main__":
     
@@ -51,21 +81,9 @@ if __name__ == "__main__":
     # Light property
     light_dir = Vector_3D(0, -1, -1)
 
-    if texture_filename is None:
-        texture_image = None
-    else:
-        texture_image = TinyImage()
-        texture_image.load_image(texture_filename)
-      
-    print("Reading facedata ...")
-    face_id_data = get_model_face_ids(obj_filename)
-
-    print("Reading vertices ...")
-    vertices, bbox = get_vertices(obj_filename)
-
-    if not texture_filename is None:
-        print("Reading texture coordinates ...")
-        texture_points = get_model_texture_points(obj_filename)
+    
+    print("Reading modeldata ...")
+    mdl = Model_Storage("autumn", obj_filename, texture_filename)
     
     zbuffer = [[-float('Inf') for bx in range(w)] for y in range(h)]
 
@@ -73,7 +91,7 @@ if __name__ == "__main__":
 
     # Generate model transformation matrix which transforms vertices according to the model bounding box
     # min[-1, -1, -1] to max[1, 1, 1] object space
-    M_model = gl.model_transform(bbox[0], bbox[1])
+    M_model = gl.model_transform(mdl.bbox[0], mdl.bbox[1])
 
     # Generate cam transformation
     M_lookat = gl.lookat(eye, center, up)
@@ -93,22 +111,22 @@ if __name__ == "__main__":
     # Iterate model faces
     print("Drawing triangles ...")
 
-    for face in progressbar(face_id_data, ):
+    for face in progressbar(mdl.face_id_data):
         vert_ids = face.VertexIds
 
-        v0 = vertices[vert_ids.id_one - 1]
-        v1 = vertices[vert_ids.id_two - 1]
-        v2 = vertices[vert_ids.id_three - 1]
+        v0 = mdl.vertices[vert_ids.id_one - 1]
+        v1 = mdl.vertices[vert_ids.id_two - 1]
+        v2 = mdl.vertices[vert_ids.id_three - 1]
 
         v0 = transform_vertex(v0, M)
         v1 = transform_vertex(v1, M)
         v2 = transform_vertex(v2, M)
 
-        if not texture_image is None:
+        if not mdl.diffuse_map is None:
             texture_pt_ids = face.TexturePointIds
-            p0 = texture_points[texture_pt_ids.id_one]
-            p1 = texture_points[texture_pt_ids.id_two]
-            p2 = texture_points[texture_pt_ids.id_three]
+            p0 = mdl.diffuse_points[texture_pt_ids.id_one - 1]
+            p1 = mdl.diffuse_points[texture_pt_ids.id_two - 1]
+            p2 = mdl.diffuse_points[texture_pt_ids.id_three -1]
         else:
             p0 = None
             p1 = None
@@ -122,7 +140,7 @@ if __name__ == "__main__":
         cos_phi = 0 if cos_phi < 0 else cos_phi
         
         image = gl.draw_triangle(v0, v1, v2, zbuffer,
-                                 p0, p1, p2, texture_image, cos_phi, 
+                                 p0, p1, p2, mdl.diffuse_map, cos_phi, 
                                  image)
 
     image.save_to_disk(output_filename)
