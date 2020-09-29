7f656cdeace2e5299554e43893e4ccbfe064c479
Christian Weihsbach <rrap1ide@gmail.com>
2020-09-29 19:06:12 +0200

    Implement storing  normal map in Model class

--- model.py
+++ model.py
@@ -6,12 +6,17 @@ from collections import namedtuple
 from tiny_image import TinyImage
 
 from numpy import array
+from enum import Enum
 from geom import Vector_3D, Point_2D, comp_min, comp_max
 
 VertexIds = namedtuple("VertexIds", "id_one id_two id_three")
 DiffusePointIds = namedtuple("DiffusePointIds", "id_one id_two id_three")
 NormalIds = namedtuple("NormalIds", "id_one id_two id_three")
 FacedataIds = namedtuple("FacedataIds", "VertexIds DiffusePointIds NormalIds")
+
+class NormalMapType(Enum):
+    GLOBAL = 1
+    TANGENT = 2
 
 def get_model_face_ids(obj_filename):
 
@@ -136,7 +141,15 @@ class Model_Storage():
     diffuse_map_w = 0
     diffuse_map_h = 0
 
-    def __init__(self, object_name: str, obj_filename: str, diffuse_map_filename: str, normal_map_filename: str):
+    normal_map_type = NormalMapType.GLOBAL
+    normal_map = None
+    normal_map_w = 0
+    normal_map_h = 0
+
+    def __init__(self, object_name: str = None, obj_filename: str = None, 
+                 diffuse_map_filename: str = None, 
+                 normal_map_filename: str = None, normal_map_type = NormalMapType.GLOBAL):
+
         self.face_id_data = get_model_face_ids(obj_filename)
         (self.vertices, self.bbox) = get_vertices(obj_filename)
         
@@ -147,9 +160,15 @@ class Model_Storage():
             self.diffuse_map.load_image(diffuse_map_filename)
             self.diffuse_map_w = self.diffuse_map.get_width()
             self.diffuse_map_h = self.diffuse_map.get_height()
-        
-        if normal_map_filename is None:
+
+        # Load normal map
+        if not normal_map_filename is None:
+            self.normal_map_type = normal_map_type
             self.normals = get_normals(obj_filename)
+            self.normal_map = TinyImage()
+            self.normal_map.load_image(normal_map_filename)
+            self.normal_map_w = self.normal_map.get_width()
+            self.normal_map_h = self.normal_map.get_height()
 
     def get_normal(self, face_idx, face_vertex_idx):
         normal_idx = self.face_id_data[face_idx].NormalIds[face_vertex_idx]
@@ -159,12 +178,21 @@ class Model_Storage():
         vertex_idx = self.face_id_data[face_idx].VertexIds[face_vertex_idx]
         return self.vertices[vertex_idx]
 
-    def get_diffuse_map_point(self, face_idx, face_vertex_idx):
+    def get_uv_map_point(self, face_idx, face_vertex_idx):
         diffuse_idx = self.face_id_data[face_idx].DiffusePointIds[face_vertex_idx]
         return self.diffuse_points[diffuse_idx]
 
     def get_diffuse_color(self, rel_x, rel_y):
         return Vector_3D(*self.diffuse_map.get(int(rel_x * self.diffuse_map_w), int(rel_y * self.diffuse_map_h)))
+    
+    def get_normal_from_map(self, rel_x, rel_y):
+        if self.normal_map_type == NormalMapType.GLOBAL:
+            return Vector_3D(*self.normal_map.get(int(rel_x * self.normal_map_w), 
+                                                  int(rel_y * self.normal_map_h))).norm()
+        elif self.normal_map_type == NormalMapType.TANGENT:
+            return None
+        else:
+            return None
     
     def get_vertex_count(self):
         return len(self.vertices)
