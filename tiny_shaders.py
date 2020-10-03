8f7b096cf662dbd4be097c20bf4c51430bfb4c14
Christian Weihsbach <rrap1ide@gmail.com>
2020-10-03 20:51:18 +0200

    Fix pylint warnings in tiny_shaders

--- tiny_shaders.py
+++ tiny_shaders.py
@@ -1,11 +1,18 @@
+"""This modules contains all tiny shaders. Shaders all have the
+   same structure maintained by an abstract base class in our_gl module."""
+
+import math
 import our_gl as gl
-from geom import Matrix_4D, Matrix_3D, Matrix_uv, Vector_3D, Barycentric, Point_2D, Point_UV, transform_vertex_to_screen, cross_product, matmul, transpose, \
-                 Vector_4D_Type, transform_3D4D3D, Vector_4D, comp_min, unpack_nested_iterable_to_list
+from geom import Matrix_4D, Matrix_3D, Matrix_uv, \
+                 Vector_4D_Type, Vector_3D, Barycentric, Point_2D, Point_UV, \
+                 transform_3D4D3D, transform_vertex_to_screen, \
+                 cross_product, matmul, transpose, \
+                 comp_min, unpack_nested_iterable_to_list
 
 from model import Model_Storage, NormalMapType
-import math
 
-class Flat_Shader(gl.Shader):
+class FlatShader(gl.Shader):
+    """Shader which creates a paperfold effect."""
     mdl: Model_Storage
     varying_vertex = [Vector_3D(0,0,0)] * 3 # Written by vertex shader, read by fragment shader
     light_dir: Vector_3D
@@ -23,7 +30,7 @@ class Flat_Shader(gl.Shader):
 
         # Get normal vector of triangle; should only be done at last vertex
         if vert_idx == 2:
-            self.n = cross_product(self.varying_vertex[2] - self.varying_vertex[0], 
+            self.n = cross_product(self.varying_vertex[2] - self.varying_vertex[0],
                                    self.varying_vertex[0] - self.varying_vertex[1]).norm()
         else:
             self.n = None
@@ -40,7 +47,8 @@ class Flat_Shader(gl.Shader):
             color = (Vector_3D(255, 255, 255) * cos_phi) // 1
             return (False, color) # Do not discard pixel and return color
 
-class Gouraud_Shader(gl.Shader):
+class GouraudShader(gl.Shader):
+    """Shader which interpolates normals of triangle vertices."""
     mdl: Model_Storage
     varying_intensity = [None] * 3 # Written by vertex shader, read by fragment shader
     light_dir: Vector_3D
@@ -53,19 +61,30 @@ class Gouraud_Shader(gl.Shader):
 
     def vertex(self, face_idx: int, vert_idx: int):
         n = self.mdl.get_normal(face_idx, vert_idx)
-        self.varying_intensity[vert_idx] = max(0, n.tr() * self.light_dir) # Get diffuse lighting intensity
-        vertex = self.mdl.get_vertex(face_idx, vert_idx) # Read the vertex
-        
-        return transform_vertex_to_screen(vertex, self.M) # Transform it to screen coordinates
+
+        # Get diffuse lighting intensity
+        self.varying_intensity[vert_idx] = max(0, n.tr() * self.light_dir)
+
+         # Read the vertex data and return
+        vertex = self.mdl.get_vertex(face_idx, vert_idx)
+
+        # Transform it to screen coordinates
+        return transform_vertex_to_screen(vertex, self.M)
 
     def fragment(self, barycentric: tuple):
         intensity = self.varying_intensity[0]*barycentric[0] \
                   + self.varying_intensity[1]*barycentric[1] \
-                  + self.varying_intensity[2]*barycentric[2] # Interpolate intensity for the current pixel
-        color = (Vector_3D(255, 255, 255) * intensity) // 1
-        return (False, color) # Do not discard pixel and return color
+                  + self.varying_intensity[2]*barycentric[2]
 
-class Gouraud_Shader_Segregated(gl.Shader):
+        # Interpolate intensity for the current pixel
+        color = (Vector_3D(255, 255, 255) * intensity) // 1
+
+        # Do not discard pixel and return color
+        return (False, color)
+
+class GouraudShaderSegregated(gl.Shader):
+    """Gouraud shader with distinct, segregated grey tones."""
+
     mdl: Model_Storage
     varying_intensity = [None] * 3 # Written by vertex shader, read by fragment shader
     light_dir: Vector_3D
@@ -80,26 +99,34 @@ class Gouraud_Shader_Segregated(gl.Shader):
 
     def vertex(self, face_idx: int, vert_idx: int):
         n = self.mdl.get_normal(face_idx, vert_idx)
-        self.varying_intensity[vert_idx] = max(0, n.tr() * self.light_dir) # Get diffuse lighting intensity
+
+        # Get diffuse lighting intensity
+        self.varying_intensity[vert_idx] = max(0, n.tr() * self.light_dir)
         vertex = self.mdl.get_vertex(face_idx, vert_idx) # Read the vertex
-        
-        return transform_vertex_to_screen(vertex, self.M) # Transform it to screen coordinates
+
+        # Transform it to screen coordinates
+        return transform_vertex_to_screen(vertex, self.M)
 
     def fragment(self, barycentric: tuple):
+        # Interpolate intensity for current pixel
         intensity = self.varying_intensity[0]*barycentric[0] \
                   + self.varying_intensity[1]*barycentric[1] \
-                  + self.varying_intensity[2]*barycentric[2] # Interpolate intensity for the current pixel
-        
+                  + self.varying_intensity[2]*barycentric[2]
+
         # Segregates intensity values to n = 'segregate_count' distinct values
         intensity = round(intensity * self.segregate_count) / self.segregate_count
-        
-        color = (Vector_3D(255, 255, 255) * intensity) // 1
-        return (False, color) # Do not discard pixel and return color
 
-class Diffuse_Gouraud_Shader(gl.Shader):
+        color = (Vector_3D(255, 255, 255) * intensity) // 1
+
+        # Do not discard pixel and return color
+        return (False, color)
+
+class DiffuseGouraudShader(gl.Shader):
+    """Shader which combines Gouraud shading and diffuse texture color."""
     mdl: Model_Storage
-    varying_intensity = [None] * 3 # Written by vertex shader, read by fragment shader
-    
+
+    # Written by vertex shader, read by fragment shader: varying_data
+    varying_intensity = [None] * 3
     # Points in varying_uv are stacked row-wise, 3 rows x 2 columns
     varying_uv = [Point_2D(0,0)] * 3
     varying_uv_shape = (3,2)
@@ -114,16 +141,24 @@ class Diffuse_Gouraud_Shader(gl.Shader):
 
     def vertex(self, face_idx: int, vert_idx: int):
         n = self.mdl.get_normal(face_idx, vert_idx)
-        self.varying_intensity[vert_idx] = max(0, n.tr() * self.light_dir) # Get diffuse lighting intensity
-        vertex = self.mdl.get_vertex(face_idx, vert_idx) # Read the vertex
-        
-        self.varying_uv[vert_idx] = self.mdl.get_uv_map_point(face_idx, vert_idx) # Get uv map point for diffuse color interpolation
-        return transform_vertex_to_screen(vertex, self.M) # Transform it to screen coordinates
+
+         # Get diffuse lighting intensity
+        self.varying_intensity[vert_idx] = max(0, n.tr() * self.light_dir)
+
+        # Read the vertex
+        vertex = self.mdl.get_vertex(face_idx, vert_idx)
+
+        # Get uv map point for diffuse color interpolation
+        self.varying_uv[vert_idx] = self.mdl.get_uv_map_point(face_idx, vert_idx)
+
+        # Transform it to screen coordinates
+        return transform_vertex_to_screen(vertex, self.M)
 
     def fragment(self, barycentric: tuple):
+        # Interpolate intensity for the current pixel
         intensity = self.varying_intensity[0]*barycentric[0] \
                   + self.varying_intensity[1]*barycentric[1] \
-                  + self.varying_intensity[2]*barycentric[2] # Interpolate intensity for the current pixel
+                  + self.varying_intensity[2]*barycentric[2]
 
         # For interpolation with barycentric coordinates we need a 2 rows x 3 columns matrix
         transposed_uv, tr_uv_shape = transpose(self.varying_uv, self.varying_uv_shape)
@@ -133,11 +168,14 @@ class Diffuse_Gouraud_Shader(gl.Shader):
 
         color = self.mdl.get_diffuse_color(p_uv.x, p_uv.y)
         color = (color * intensity) // 1
-        return (False, color) # Do not discard pixel and return color
 
-class Global_Normalmap_Shader(gl.Shader):
+        # Do not discard pixel and return color
+        return (False, color)
+
+class GlobalNormalmapShader(gl.Shader):
+    """Shader reading a global space normal map to increase detail."""
     mdl: Model_Storage
-    
+
     # Points in varying_uv are stacked row-wise, 3 rows x 2 columns
     varying_uv = [Point_2D(0,0)] * 3
 
@@ -157,10 +195,14 @@ class Global_Normalmap_Shader(gl.Shader):
         self.uniform_M_pe_IT = M_pe_IT
 
     def vertex(self, face_idx: int, vert_idx: int):
-        vertex = self.mdl.get_vertex(face_idx, vert_idx) # Read the vertex
-        
-        self.varying_uv[vert_idx] = self.mdl.get_uv_map_point(face_idx, vert_idx) # Get uv map point for diffuse color interpolation
-        return transform_vertex_to_screen(vertex, self.uniform_M_sc) # Transform it to screen coordinates
+        # Read the vertex
+        vertex = self.mdl.get_vertex(face_idx, vert_idx)
+
+        # Get uv map point for diffuse color interpolation
+        self.varying_uv[vert_idx] = self.mdl.get_uv_map_point(face_idx, vert_idx)
+
+         # Transform it to screen coordinates
+        return transform_vertex_to_screen(vertex, self.uniform_M_sc)
 
     def fragment(self, barycentric: tuple):
         # For interpolation with barycentric coordinates we need a 2 rows x 3 columns matrix
@@ -171,16 +213,22 @@ class Global_Normalmap_Shader(gl.Shader):
 
         n = self.mdl.get_normal_from_map(p_uv.x, p_uv.y)
         n = transform_3D4D3D(n, Vector_4D_Type.DIRECTION, self.uniform_M_pe_IT).norm()
-        l = transform_3D4D3D(self.uniform_light_dir, Vector_4D_Type.DIRECTION, self.uniform_M_pe).norm()
-        intensity = max(0, n.tr() * l) # Get diffuse lighting intensity
+        l = transform_3D4D3D(self.uniform_light_dir, Vector_4D_Type.DIRECTION,
+                             self.uniform_M_pe).norm()
 
-        color = Vector_3D(255,255,255)#self.mdl.get_diffuse_color(p_uv.x, p_uv.y)
+        # Get diffuse lighting intensity
+        intensity = max(0, n.tr() * l)
+
+        color = self.mdl.get_diffuse_color(p_uv.x, p_uv.y)
         color = (color * intensity) // 1
-        return (False, color) # Do not discard pixel and return color
 
-class Specularmap_Shader(gl.Shader):
+        # Do not discard pixel and return color
+        return (False, color)
+
+class SpecularmapShader(gl.Shader):
+    """Shader combining global normal map shading and specular lighting."""
     mdl: Model_Storage
-    
+
     # Points in varying_uv are stacked row-wise, 3 rows x 2 columns
     varying_uv = [Point_2D(0,0)] * 3
 
@@ -200,10 +248,14 @@ class Specularmap_Shader(gl.Shader):
         self.uniform_M_pe_IT = M_pe_IT
 
     def vertex(self, face_idx: int, vert_idx: int):
-        vertex = self.mdl.get_vertex(face_idx, vert_idx) # Read the vertex
-        
-        self.varying_uv[vert_idx] = self.mdl.get_uv_map_point(face_idx, vert_idx) # Get uv map point for diffuse color interpolation
-        return transform_vertex_to_screen(vertex, self.uniform_M_sc) # Transform it to screen coordinates
+        # Read the vertex
+        vertex = self.mdl.get_vertex(face_idx, vert_idx)
+
+        # Get uv map point for diffuse color interpolation
+        self.varying_uv[vert_idx] = self.mdl.get_uv_map_point(face_idx, vert_idx)
+
+        # Transform it to screen coordinates
+        return transform_vertex_to_screen(vertex, self.uniform_M_sc)
 
     def fragment(self, barycentric: tuple):
         # For interpolation with barycentric coordinates we need a 2 rows x 3 columns matrix
@@ -214,11 +266,13 @@ class Specularmap_Shader(gl.Shader):
         n = transform_3D4D3D(n, Vector_4D_Type.DIRECTION, self.uniform_M_pe_IT).norm()
         l = transform_3D4D3D(self.uniform_light_dir, Vector_4D_Type.DIRECTION, self.uniform_M_pe).norm()
         n_l = n.tr() * l
-        diffuse_intensity = max(0, n_l) # Get diffuse lighting intensity
+
+        # Get diffuse lighting intensity
+        diffuse_intensity = max(0, n_l)
 
         # Reflected light direction (already transformed as n and l got transformed)
-        r = (2 * (n_l) * n - l).norm()
-        cos_r_z = max(0, r.z) # r.tr() * Vector_3D(0, 0, 1) == r.z
+        reflect = (2 * (n_l) * n - l).norm()
+        cos_r_z = max(0, reflect.z) # equals: reflect.tr() * Vector_3D(0, 0, 1) == reflect.z
         specular_intensity = math.pow(cos_r_z, self.mdl.get_specular_power_from_map(*p_uv))
 
         color = self.mdl.get_diffuse_color(*p_uv)
@@ -226,11 +280,14 @@ class Specularmap_Shader(gl.Shader):
         # Combine base, diffuse and specular intensity
         color = 10 * Vector_3D(1, 1, 1) + (diffuse_intensity + 0.5 * specular_intensity) * color
         color = comp_min(Vector_3D(255, 255, 255), color) // 1
-        return (False, color) # Do not discard pixel and return color
 
-class Tangent_Normalmap_Shader(gl.Shader):
+        # Do not discard pixel and return color
+        return (False, color)
+
+class TangentNormalmapShader(gl.Shader):
+    """Shader equal to GlobalNormalmapShader but with tangent space normal map."""
     mdl: Model_Storage
-    
+
     # Points in varying_uv are stacked row-wise, 3 rows x 2 columns
     varying_uv = Matrix_uv([0]*6)
     varying_vert = [Vector_3D(0,0,0)] * 3
@@ -257,13 +314,15 @@ class Tangent_Normalmap_Shader(gl.Shader):
         vertex = transform_3D4D3D(vertex, Vector_4D_Type.POINT, self.uniform_M_pe)
         self.varying_vert[vert_idx] = vertex
 
-        # Store triangle vertex normal (after being transformed to perspective)
         nvert = self.mdl.get_normal(face_idx, vert_idx) # Read normal of the vertex
         nvert = transform_3D4D3D(nvert, Vector_4D_Type.DIRECTION, self.uniform_M_pe_IT).norm()
-        self.varying_nvert[vert_idx] = nvert # Already projected onto screen plane
+
+        # Store triangle vertex normal (after being transformed to perspective)
+        self.varying_nvert[vert_idx] = nvert
 
         # Get uv map point for diffuse color interpolation and store it
-        self.varying_uv = self.varying_uv.set_col(vert_idx, self.mdl.get_uv_map_point(face_idx, vert_idx))
+        self.varying_uv = \
+            self.varying_uv.set_col(vert_idx, self.mdl.get_uv_map_point(face_idx, vert_idx))
 
         # Transform it to screen coordinates
         return transform_vertex_to_screen(vertex, self.uniform_M_viewport)
@@ -273,7 +332,9 @@ class Tangent_Normalmap_Shader(gl.Shader):
         p = self.varying_uv * bary
         p_uv = Point_UV(*p)
 
-        transposed_nvert, tr_nvert_shape = transpose(unpack_nested_iterable_to_list(self.varying_nvert), (3,3))
+        transposed_nvert, tr_nvert_shape = \
+            transpose(unpack_nested_iterable_to_list(self.varying_nvert), (3,3))
+
         n_bary, _ = matmul(transposed_nvert, tr_nvert_shape, barycentric, (3,1))
         n_bary = Vector_3D(*n_bary).norm()
 
