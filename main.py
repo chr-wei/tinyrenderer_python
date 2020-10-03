3cce0274989ef77592ff67fa4b48515053ea65c9
Christian Weihsbach <rrap1ide@gmail.com>
2020-10-03 18:52:24 +0200

    Make main pylint ready

--- main.py
+++ main.py
@@ -1,43 +1,44 @@
-import numpy as np
+"""A tiny shader fork written in Python 3"""
+
 from progressbar import progressbar
 
 from tiny_image import TinyImage
 
 import our_gl as gl
-from geom import Vector_3D, cross_product
-from model import Model_Storage, get_model_face_ids, get_vertices, NormalMapType
-from tiny_shaders import Flat_Shader, Gouraud_Shader, Gouraud_Shader_Segregated, Diffuse_Gouraud_Shader, \
-                         Global_Normalmap_Shader, Specularmap_Shader, Tangent_Normalmap_Shader
+from geom import Vector_3D
+from model import Model_Storage, NormalMapType
+from tiny_shaders import Flat_Shader, Gouraud_Shader, Gouraud_Shader_Segregated, \
+                         Diffuse_Gouraud_Shader, Global_Normalmap_Shader, Specularmap_Shader, \
+                         Tangent_Normalmap_Shader
 
 if __name__ == "__main__":
-    
     # Model property selection
-    model_prop_set = 1
-    if model_prop_set == 0:
-        obj_filename = "obj/autumn/autumn.obj"
-        diffuse_filename = "obj/autumn/TEX_autumn_body_color.png"
-        normal_map_filename = "obj/autumn/TEX_autumn_body_normals_wrld_space.tga"
-        normal_map_type = NormalMapType.GLOBAL
-        specular_map_filename = "obj/autumn/TEX_autumn_body_spec.tga"
-        output_filename = "renders/out.png"
-    elif model_prop_set == 1:
-        obj_filename = "obj/head/head.obj"
-        diffuse_filename = "obj/head/head_diffuse.tga"
-        normal_map_filename = "obj/head/head_nm_tangent.tga"
-        normal_map_type = NormalMapType.TANGENT
-        specular_map_filename = "obj/head/head_spec.tga"
-        output_filename = "renders/out.png"
+    MODEL_PROP_SET = 1
+    if MODEL_PROP_SET == 0:
+        OBJ_FILENAME = "obj/autumn/autumn.obj"
+        DIFFUSE_FILENAME = "obj/autumn/TEX_autumn_body_color.png"
+        NORMAL_MAP_FILENAME = "obj/autumn/TEX_autumn_body_normals_wrld_space.tga"
+        NORMAL_MAP_TYPE = NormalMapType.GLOBAL
+        SPECULAR_MAP_FILENAME = "obj/autumn/TEX_autumn_body_spec.tga"
+        OUTPUT_FILENAME = "renders/out.png"
+    elif MODEL_PROP_SET == 1:
+        OBJ_FILENAME = "obj/head/head.obj"
+        DIFFUSE_FILENAME = "obj/head/head_diffuse.tga"
+        NORMAL_MAP_FILENAME = "obj/head/head_nm_tangent.tga"
+        NORMAL_MAP_TYPE = NormalMapType.TANGENT
+        SPECULAR_MAP_FILENAME = "obj/head/head_spec.tga"
+        OUTPUT_FILENAME = "renders/out.png"
     else:
-        obj_filename = "obj/head/head.obj"
-        diffuse_filename = "obj/head/head_diffuse.tga"
-        normal_map_filename = "obj/head/head_nm.tga"
-        normal_map_type = NormalMapType.GLOBAL
-        specular_map_filename = "obj/head/head_spec.tga"
-        output_filename = "renders/out.png"
-    
+        OBJ_FILENAME = "obj/head/head.obj"
+        DIFFUSE_FILENAME = "obj/head/head_diffuse.tga"
+        NORMAL_MAP_FILENAME = "obj/head/head_nm.tga"
+        NORMAL_MAP_TYPE = NormalMapType.GLOBAL
+        SPECULAR_MAP_FILENAME = "obj/head/head_spec.tga"
+        OUTPUT_FILENAME = "renders/out.png"
+
     # Image property selection
-    img_prop_set = 1
-    if img_prop_set == 0:
+    IMG_PROP_SET = 1
+    if IMG_PROP_SET == 0:
         (w, h) = (2000, 2000)
     else:
         (w, h) = (800, 800)
@@ -45,47 +46,48 @@ if __name__ == "__main__":
     image = TinyImage(w, h)
 
     # View property selection
-    view_prop_set = 1
-    if view_prop_set == 0:
-        eye = Vector_3D(0, 0, 1) # Lookat camera 'eye' position
-        center = Vector_3D(0, 0, 0) # Lookat 'center'. 'eye' looks at center
-        up = Vector_3D(0, 1, 0) # Camera 'up' direction
-        scale = .8 # Viewport scaling
-    elif view_prop_set == 1:
-        eye = Vector_3D(1, 0, 1)
-        center = Vector_3D(0, 0, 0)
-        up = Vector_3D(0, 1, 0)
-        scale = .8
+    VIEW_PROP_SET = 1
+    if VIEW_PROP_SET == 0:
+        EYE = Vector_3D(0, 0, 1) # Lookat camera 'EYE' position
+        CENTER = Vector_3D(0, 0, 0) # Lookat 'CENTER'. 'EYE' looks at CENTER
+        UP = Vector_3D(0, 1, 0) # Camera 'UP' direction
+        SCALE = .8 # Viewport scaling
+    elif VIEW_PROP_SET == 1:
+        EYE = Vector_3D(1, 0, 1)
+        CENTER = Vector_3D(0, 0, 0)
+        UP = Vector_3D(0, 1, 0)
+        SCALE = .8
     else:
-        eye = Vector_3D(1, 0, 0) # Lookat camera 'eye' position
-        center = Vector_3D(0, 0, 0) # Lookat 'center'. 'eye' looks at center
-        up = Vector_3D(0, 1, 0) # Camera 'up' direction
-        scale = .8 # Viewport scaling
+        EYE = Vector_3D(1, 0, 0) # Lookat camera 'EYE' position
+        CENTER = Vector_3D(0, 0, 0) # Lookat 'CENTER'. 'EYE' looks at CENTER
+        UP = Vector_3D(0, 1, 0) # Camera 'UP' direction
+        SCALE = .8 # Viewport scaling
 
     # Light property
-    light_dir = Vector_3D(1, 0, 1).norm()
+    LIGHT_DIR = Vector_3D(1, 0, 1).norm()
 
-    
     print("Reading modeldata ...")
-    mdl = Model_Storage(object_name = "autumn", obj_filename = obj_filename, 
-                        diffuse_map_filename = diffuse_filename, 
-                        normal_map_filename=normal_map_filename, normal_map_type = normal_map_type,
-                        specular_map_filename=specular_map_filename)
+    mdl = Model_Storage(object_name = "autumn", obj_filename=OBJ_FILENAME,
+                        diffuse_map_filename=DIFFUSE_FILENAME,
+                        normal_map_filename=NORMAL_MAP_FILENAME, normal_map_type=NORMAL_MAP_TYPE,
+                        specular_map_filename=SPECULAR_MAP_FILENAME)
 
     # Define tranformation matrices
 
-    # Generate model transformation matrix which transforms vertices according to the model bounding box
+    # Generate model transformation matrix which transforms
+    #     # vertices according to the model bounding box
+    #
     # min[-1, -1, -1] to max[1, 1, 1] object space
     M_model = gl.model_transform(mdl.bbox[0], mdl.bbox[1])
 
     # Generate cam transformation
-    M_lookat = gl.lookat(eye, center, up)
+    M_lookat = gl.lookat(EYE, CENTER, UP)
 
     # Generate perspective transformation
     M_perspective = gl.perspective(4.0)
 
     # Generate transformation to final viewport
-    M_viewport = gl.viewport(+scale*w/8, +scale*h/8, scale*w, scale*h, 255)
+    M_viewport = gl.viewport(+SCALE*w/8, +SCALE*h/8, SCALE*w, SCALE*h, 255)
 
     # Combine matrices
     M_modelview = M_lookat * M_model
@@ -96,21 +98,21 @@ if __name__ == "__main__":
 
     zbuffer = [[-float('Inf') for bx in range(w)] for y in range(h)]
 
-    shader_prop_set = 5
-    if shader_prop_set == 0:
-        shader = Gouraud_Shader(mdl, light_dir, M_sc)
-    elif shader_prop_set == 1:
-        shader = Gouraud_Shader_Segregated(mdl, light_dir, M_sc, 4)
-    elif shader_prop_set == 2:
-        shader = Diffuse_Gouraud_Shader(mdl, light_dir, M_sc)
-    elif shader_prop_set == 3:
-        shader = Global_Normalmap_Shader(mdl, light_dir, M_pe, M_sc, M_pe_IT)
-    elif shader_prop_set == 4:
-        shader = Specularmap_Shader(mdl, light_dir, M_pe, M_sc, M_pe_IT)
-    elif shader_prop_set == 5:
-        shader = Tangent_Normalmap_Shader(mdl, light_dir, M_pe, M_pe_IT, M_viewport)
+    SHADER_PROP_SET = 5
+    if SHADER_PROP_SET == 0:
+        shader = Gouraud_Shader(mdl, LIGHT_DIR, M_sc)
+    elif SHADER_PROP_SET == 1:
+        shader = Gouraud_Shader_Segregated(mdl, LIGHT_DIR, M_sc, 4)
+    elif SHADER_PROP_SET == 2:
+        shader = Diffuse_Gouraud_Shader(mdl, LIGHT_DIR, M_sc)
+    elif SHADER_PROP_SET == 3:
+        shader = Global_Normalmap_Shader(mdl, LIGHT_DIR, M_pe, M_sc, M_pe_IT)
+    elif SHADER_PROP_SET == 4:
+        shader = Specularmap_Shader(mdl, LIGHT_DIR, M_pe, M_sc, M_pe_IT)
+    elif SHADER_PROP_SET == 5:
+        shader = Tangent_Normalmap_Shader(mdl, LIGHT_DIR, M_pe, M_pe_IT, M_viewport)
     else:
-        shader = Flat_Shader(mdl, light_dir, M_sc)
+        shader = Flat_Shader(mdl, LIGHT_DIR, M_sc)
 
     # Iterate model faces
     print("Drawing triangles ...")
@@ -120,9 +122,9 @@ if __name__ == "__main__":
     for face_idx in progressbar(range(mdl.get_face_count())):
         for face_vert_idx in range(3):
             # Get transformed vertex and prepare internal shader data
-            screen_coords[face_vert_idx] = shader.vertex(face_idx, face_vert_idx)        
-        
+            screen_coords[face_vert_idx] = shader.vertex(face_idx, face_vert_idx)      
+
         # Rasterize triangle
         image = gl.draw_triangle(screen_coords, shader, zbuffer, image)
 
-    image.save_to_disk(output_filename)
+    image.save_to_disk(OUTPUT_FILENAME)
